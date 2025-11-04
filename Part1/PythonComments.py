f3c61ad7659ab06d38bb86fb0b40dc4950dd2582
rpankivroma <pankiv.roma@gmail.com>
2025-11-04 16:22:02 +0200

    Add: PythonComments.py

---
+++ Part1/PythonComments.py
@@ -0,0 +1,34 @@
+"""
+Request: Give me one practice task on topic Python Comments 
+Based on https://www.w3schools.com/python/python_comments.asp
+
+GPT Task: Add Comments to Explain Code
+Write a Python program that:
+1. Creates a variable x with the value 5.
+2. Creates a variable y with the value 10.
+3. Prints the sum of x and y.
+4. Add at least three comments explaining:
+    - What each variable does
+    - What the print statement does
+    - (Optional) A short description at the top of the code
+"""
+# Answer:
+
+"""
+This program returns result of the sum x and y. 
+In this case we set value for x 5 and for y 10, 
+so we expect that outcom of this program will be 15.
+"""
+
+x = 5  #variable x with velu 5
+y = 10 #variable y with velu 10
+
+print(x + y) # funktion for show off the resut (expected result is 15)
+
+
+"""
+Terminal outcom:
+PS C:\Users\trade> & C:/Users/trade/AppData/Local/Programs/Python/Python314/python.exe 
+c:/Users/trade/Desktop/PythonPractice/PythonPractice/Part1/PythonComments.py
+15
+"""
