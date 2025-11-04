794f855efa2538eb15c23316dd93da9ee6e0d94c
rpankivroma <pankiv.roma@gmail.com>
2025-11-04 14:31:06 +0200

    Add: file "PythonSyntax.py"

---
+++ Python Syntax.py
@@ -0,0 +1,22 @@
+# Request:Give me one practice task on topic Python Syntax 
+# (threory take form souse: https://www.w3schools.com/python/python_statements.asp)
+# Task from ChatGPt: Multiple Statements and Indentation
+# Write a Python program that:
+# Stores two numbers in variables a and b.
+# On one line, assigns values 10 to a and 20 to b.
+# Uses an if statement to check if b is greater than a.
+# If true, print: b is greater than a
+# Make sure your indentation is correct.
+
+# Answer:
+
+a = 10
+b = 20
+
+if b > a :
+    print("b is greater than a")
+
+# Output from terminal:
+# PS C:\Users\trade> & C:/Users/trade/AppData/Local/Programs/Python/Python314/python.exe 
+# "c:/Users/trade/Desktop/PythonPractice/PythonPractice/Python Syntax.py"
+# b is greater than a
