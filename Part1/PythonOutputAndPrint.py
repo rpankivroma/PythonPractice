7b7aaf32b1df53f06543bfcb0d3543135d9eaf40
rpankivroma <pankiv.roma@gmail.com>
2025-11-04 14:53:50 +0200

    Add: file PythonOutputPrint.py

---
+++ Part1/PythonOutputAndPrint.py
@@ -0,0 +1,21 @@
+# Request: Give me one practice task on topic Python Output / Print 
+# Based on https://www.w3schools.com/python/python_output.asp
+
+# GPT Task: Combine Text and Numbers in Output 
+# Write a Python program that: Creates a variable name with your name. 
+# Creates a variable age with your age (as a number). 
+# Prints the following sentence using the print() function: 
+# My name is <name>, and I am <age> years old. 
+# Make sure to combine text and variables correctly (hint: use commas or f-strings).
+
+# Answer:
+
+name = "Roman"
+age = 24
+
+print("My name is", name, "and I am", age, "years old.")
+
+# Output from terminal:
+# PS C:\Users\trade> & C:/Users/trade/AppData/Local/Programs/Python/Python314/python.exe
+#  c:/Users/trade/Desktop/PythonPractice/PythonPractice/Part1/PythonOutputAndPrint.py
+# My name is Roman and I am 24 years old.
