def77c34a1e02f1fd894764f5ade1d129926f842
rpankivroma <pankiv.roma@gmail.com>
2025-11-12 15:20:53 +0200

    Update Exercice_1.py

---
+++ PyNative/Exercice_1.py
@@ -0,0 +1,15 @@
+"""
+Exercise 1: Calculate the multiplication and sum of two numbers
+"""
+
+#Answer:
+
+number1 = 20
+number2 = 30
+
+print("The multiplication of 30 and 20 is:", number1 * number2)
+
+number1 = 40
+number2 = 30
+
+print("The sum of 40 and 30 is:", number1 + number2)
