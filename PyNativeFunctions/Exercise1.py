ab7e5895b1cef8fdbd8531e94f495a3a8c9e6a6d
rpankivroma <pankiv.roma@gmail.com>
2025-12-14 18:32:44 +0200

    Update Exercise1.py

---
+++ PyNativeFunctions/Exercise1.py
@@ -0,0 +1,15 @@
+"""
+Exercise 1: Create a function in Python
+Write a program to create a function that takes two arguments, name and age, and prints their values.
+"""
+
+def MyFunc(name, age):
+    print(name, age)
+
+MyFunc("Roman", 24)
+
+"""
+Terminal output:
+PS C:\Users\trade> & C:/Users/trade/AppData/Local/Programs/Python/Python314/python.exe c:/Users/trade/Desktop/PythonPractice/PythonPractice/PyNativeFunctions/Exercise1.py
+Roman 24
+"""
