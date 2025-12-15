2a59e217c8e7ad1b81a8c4780f7e20dcd8aca753
rpankivroma <pankiv.roma@gmail.com>
2025-12-15 20:08:28 +0200

    Update Exercise9.py

---
+++ PyNativeFunctions/Exercise9.py
@@ -0,0 +1,19 @@
+"""
+Exercise 9: Find the largest item from list
+Given:
+x = [4, 6, 8, 24, 12, 2]
+Expected Output:
+24
+"""
+
+# Answer
+
+x = [4, 6, 8, 24, 12, 2]
+print(max(x))
+
+"""
+Terminal output:
+PS C:\Users\trade> & C:/Users/trade/AppData/Local/Programs/Python/Python314/python.exe 
+c:/Users/trade/Desktop/PythonPractice/PythonPractice/PyNativeFunctions/Exercise9.py
+24
+"""
