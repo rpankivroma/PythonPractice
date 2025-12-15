18a0e00f5d778db174cfddfc039b92c46d1a0733
rpankivroma <pankiv.roma@gmail.com>
2025-12-15 19:47:44 +0200

    Update Exercise8.py

---
+++ PyNativeFunctions/Exercise8.py
@@ -0,0 +1,12 @@
+"""
+Exercise 8: Generate a Python list of all the even numbers between 4 to 30
+"""
+
+print(list(range(4,30,2)))
+
+"""
+Terminal output:
+PS C:\Users\trade> & C:/Users/trade/AppData/Local/Programs/Python/Python314/python.exe 
+c:/Users/trade/Desktop/PythonPractice/PythonPractice/PyNativeFunctions/Exercise8.py
+[4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28]
+"""
