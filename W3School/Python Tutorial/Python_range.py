d2b795c07f2592840b82b770bc44e10f2377bd7e
rpankivroma <pankiv.roma@gmail.com>
2025-12-22 11:31:04 +0200

    Update Python_range.py

---
+++ W3School/Python Tutorial/Python_range.py
@@ -0,0 +1,39 @@
+"""
+Python range() — Task
+
+Task Description
+Write a Python program that uses range() to generate and analyze sequences of numbers.
+
+Requirements
+1. Basic Range
+Use range() to generate numbers from 0 to 9.
+Print each number in the sequence using a for loop.
+
+Expected output:
+0
+1
+2
+...
+9
+"""
+
+r = range(10)
+
+for i in r:
+    print(i)
+
+"""
+Terminal output:
+PS C:\Users\trade> & C:/Users/trade/AppData/Local/Programs/Python/Python314/python.exe 
+"c:/Users/trade/Desktop/PythonPractice/PythonPractice/W3School/Python Tutoriol/Python Tutorial/Python_range.py"
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+"""
