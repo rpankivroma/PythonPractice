30c514ddc5a09c4d902e3c52fd2536b36cb98ff2
rpankivroma <pankiv.roma@gmail.com>
2026-01-29 08:26:04 +0200

    Update cooking_blog_mysql.sql

--- cooking_blog/cooking_blog_mysql.sql
+++ cooking_blog/cooking_blog_mysql.sql
@@ -12,14 +12,19 @@ CREATE TABLE IF NOT EXISTS recipes (
   chapter VARCHAR(100),
   image_url TEXT,
   author VARCHAR(100),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  for_sale BOOLEAN DEFAULT FALSE,
+  price DECIMAL(10,2)
 );
 
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) NOT NULL UNIQUE,
   email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
+  bank_name VARCHAR(255),
+  card_number VARCHAR(255),
+  card_holder_name VARCHAR(255)
 );
 
 INSERT INTO recipes (title, description, ingredients, steps, cooking_time, difficulty, chapter, image_url, author) VALUES
